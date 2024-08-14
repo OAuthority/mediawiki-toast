97f6aa40ae4ccbe931ccc979b2c42e0100c1bc44
OriginalAuthority <luke@telepedia.net>
2024-08-14 23:35:13 +0100

    Updates

--- resources/ext.mediawiki.toast/mediawiki-toast.js
+++ resources/ext.mediawiki.toast/mediawiki-toast.js
@@ -1,5 +1,16 @@
-(function () {
+( function (  ) {
+	'use strict';
 
+	/**
+	 * @type {{
+	 *   duration: number,
+	 *   showClose: boolean,
+	 *   colourful: boolean,
+	 *   position: string,
+	 *   type: string,
+	 *   autoClose: boolean
+	 * }}
+	 */
 	Toast.defaults = {
 		autoClose: true,
 		colourful: false,
@@ -9,24 +20,100 @@
 		type: "notice"
 	}
 
-	function Toast(message, options = {}) {
-		this.options = {};
-		this.options.autoClose = options.autoClose ?? Toast.defaults.autoClose;
-		this.options.colourful = options.colourful ?? Toast.defaults.colourful;
-		this.options.duration = options.duration ?? Toast.defaults.duration;
-		this.options.position = options.position ?? Toast.defaults.position;
-		this.options.showClose = options.showClose ?? Toast.defaults.showClose;
-		this.options.type = options.type ?? Toast.defaults.type;
-		this.options.message = message ?? 'This is a toast notification!';
+	/**
+	 * Setup all of our options and their defaults
+	 * @param {string} message - the message to display in the notification
+	 * @param {Object} options - the options to pass
+	 * @constructor
+	 */
+	function Toast( message, options = {} ) {
+		this.options = {
+			autoClose: options.autoClose ?? Toast.defaults.autoClose,
+			colourful: options.colourful ?? Toast.defaults.colourful,
+			duration: options.duration ?? Toast.defaults.duration,
+			position: options.position ?? Toast.defaults.position,
+			showClose: options.showClose ?? Toast.defaults.showClose,
+			type: options.type ?? Toast.defaults.type,
+			message: message ?? 'This is a toast notification!'
+		};
 	}
 
-	Toast.prototype.show = function() {
-		const template = getTemplate();
+	/**
+	 * Actually show the notification, with optional extras
+	 * Usage: new Toast( 'This is a toast!', { type: "notice" }  ).show(  )
+	 */
+	Toast.prototype.show = function(  ) {
+		const template = getTemplate(  );
 		const html = template.render( this.options );
+
+		this.$element = $( html );
+
+		let container = $( '.mw-toast-notification-container.' + this.options.position );
+
+		if ( !container.length ) {
+			container = $( '<div class="mw-toast-notification-container ' + this.options.position + '"></div>' );
+			$( 'body' ).append( container );
+		}
+
+		container.append( this.$element );
+
+		if (  this.options.showClose ) {
+			initCloseNotif( this );
+		}
+
+		if (  this.options.autoClose  ) {
+			setTimeout( ( ) => {
+				this.hide( );
+			}, this.options.duration );
+		}
 	}
 
+	/**
+	 * Hide the toast ( remove it from the DOM )
+	 */
+	Toast.prototype.hide = function(  ) {
+		if ( this.$element ) {
+			const slideDirection = getSlideDirection( this.options.position );
+			const animationCSS = {};
+			animationCSS[slideDirection] = '-100%';
+
+			this.$element.animate( animationCSS, 400, () => {
+				this.$element.remove();
+			});
+		}
+	};
+
+	/**
+	 * Get the mustache template; MediaWiki will handle this internally, so just retrieve it
+	 * @returns {*}
+	 */
 	function getTemplate() {
-		return mw.template.get('ext.mediawiki.toast', 'notice.mustache');
+		return mw.template.get( 'ext.mediawiki.toast', 'notice.mustache' );
+	}
+
+	/**
+	 * If we are showing the close button, delete the notification after it is clicked
+	 * @param {Toast} toast
+	 */
+	function initCloseNotif( toast ) {
+		const close = toast.$element.find( '.mw-toast-notification__close' );
+
+		close.on('click', function( e ) {
+			console.log( 'clicked close' );
+			toast.hide();
+		} );
+	}
+
+	/**
+	 * Work out whether we should fade to the left or right based on the position of
+	 * the toast on the screen
+	 * @param {string} position
+	 * @returns {string}
+	 */
+	function getSlideDirection( position) {
+		if ( position.includes( 'left' ) ) return 'left';
+		if ( position.includes( 'right' ) ) return 'right';
+		return 'right';
 	}
 
 	window.Toast = Toast;
