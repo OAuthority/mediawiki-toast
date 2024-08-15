1075cfee4ab3d47ee4670aeaf513ca27738aed7a
OriginalAuthority <luke@telepedia.net>
2024-08-15 02:06:05 +0100

    fixes for eslint

--- resources/ext.mediawiki.toast/mediawiki-toast.js
+++ resources/ext.mediawiki.toast/mediawiki-toast.js
@@ -20,9 +20,9 @@
 		autoClose: true,
 		colourful: false,
 		duration: 3000,
-		position: "top-right",
+		position: 'top-right',
 		showClose: true,
-		type: "notice",
+		type: 'notice',
 		icon: notice
 	};
 
@@ -40,6 +40,7 @@
 
 	/**
 	 * Setup all of our options and their defaults
+	 *
 	 * @param {string} message - the message to display in the notification
 	 * @param {Object} options - the options to pass
 	 * @constructor
@@ -54,16 +55,15 @@
 			showClose: options.showClose ?? Toast.defaults.showClose,
 			type: options.type ?? Toast.defaults.type,
 			message: message ?? 'This is a toast notification!',
-			icon: Toast.types[options.type]?.icon ?? Toast.defaults.icon
+			icon: Toast.types[ options.type ]?.icon ?? Toast.defaults.icon
 		};
 	}
-
 
 	/**
 	 * Actually show the notification, with optional extras
 	 * Usage: new Toast( 'This is a toast!', { type: "notice" } ).show(  )
 	 */
-	Toast.prototype.show = function() {
+	Toast.prototype.show = function () {
 		const template = getTemplate();
 		const html = template.render( this.options );
 
@@ -78,13 +78,13 @@
 
 		container.append( this.$element );
 
-		if (  this.options.autoClose  ) {
+		if ( this.options.autoClose ) {
 			setTimeout( () => {
 				this.hide();
 			}, this.options.duration );
 		}
 
-		if (  this.options.showClose ) {
+		if ( this.options.showClose ) {
 			initCloseNotif( this );
 		}
 	};
@@ -92,21 +92,22 @@
 	/**
 	 * Hide the toast ( remove it from the DOM )
 	 */
-	Toast.prototype.hide = function() {
+	Toast.prototype.hide = function () {
 		if ( this.$element ) {
 			const slideDirection = getSlideDirection( this.options.position );
 			const animationCSS = {};
-			animationCSS[slideDirection] = '-100%';
+			animationCSS[ slideDirection ] = '-100%';
 
 			this.$element.animate( animationCSS, 400, () => {
 				this.$element.remove();
-			});
+			} );
 		}
 	};
 
 	/**
 	 * Get the mustache template; MediaWiki will handle this internally, so just retrieve it
-	 * @returns {*}
+	 *
+	 * @return {*}
 	 */
 	function getTemplate() {
 		return mw.template.get( 'ext.mediawiki.toast', 'notice.mustache' );
@@ -114,12 +115,13 @@
 
 	/**
 	 * If we are showing the close button, delete the notification after it is clicked
+	 *
 	 * @param {Toast} toast
 	 */
 	function initCloseNotif( toast ) {
 		const close = toast.$element.find( '.mw-toast-notification__close' );
 
-		close.on('click', function( e ) {
+		close.on( 'click', function () {
 			toast.hide();
 		} );
 	}
@@ -127,15 +129,14 @@
 	/**
 	 * Work out whether we should fade to the left or right based on the position of
 	 * the toast on the screen
+	 *
 	 * @param {string} position
-	 * @returns {string}
+	 * @return {string}
 	 */
-	function getSlideDirection( position) {
-		if ( position.includes( 'left' ) ) return 'left';
-		if ( position.includes( 'right' ) ) return 'right';
-		return 'right';
+	function getSlideDirection( position ) {
+		return position.includes( 'left' ) ? 'left' : 'right';
 	}
 
 	window.Toast = Toast;
 
-})();
+}() );
